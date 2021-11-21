82fd48b0e0b99f050d8572939ce9ae7f0241ea48
深澤　寛太 <kantafkzw@gmail.com>
2021-11-21 19:51:46 +0900

    テーブルを作成しデータを挿入

---
+++ db/create_users.sql
@@ -0,0 +1,7 @@
+create table users ( 
+id int,
+last_name varchar(255),
+first_name varchar(255),
+email varchar(255),
+gender varchar(255)
+);
---
+++ db/insert_users.sql
@@ -0,0 +1,8 @@
+INSERT 
+  INTO users 
+  VALUES 
+    (1, '田中', '太郎', 'tanaka@sample.com', 'male'),
+    (2, '山田', '次郎', 'yamada@sample.com', 'male'),
+    (3, '吉田', '姫子', 'yoshida@sample.com', 'female'),
+    (4, '山口', '雄二', 'yuji@sample.com', 'male'),
+    (5, '杉本', '佳子', NULL, 'female');
